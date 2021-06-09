7e125c194c3a1484585f5832c42701dd4cee0ee8
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-09 08:33:49 -0400

    commit tree a5bd0b48cafe6591b27a24a6e72c4ddd0a14b463

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+    const mappedArr = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        mappedArr.push(sourceArray[i] * -1);
+    }
+    return mappedArr;
+}
+
+const mapToNoChange = sourceArr => sourceArr;
+
+const mapToDouble = sourceArr => {
+    const mappedArr = [];
+    for (let i = 0; i < sourceArr.length; i++) {
+        mappedArr.push(sourceArr[i] * 2);
+    }
+    return mappedArr;
+}
+
+const mapToSquare = sourceArr => {
+    const mappedArr = [];
+    for (let i = 0; i < sourceArr.length; i++) {
+        mappedArr.push(sourceArr[i] ** 2);
+    }
+    return mappedArr;
+}
+
+const reduceToTotal = (sourceArray, startingPoint = 0) => {
+    let total = startingPoint;
+    for (let i = 0; i < sourceArray.length; i++) {
+        total += sourceArray[i];
+    }
+    return total;
+}
+
+const reduceToAllTrue = sourceArray => {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const reduceToAnyTrue = sourceArray => {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] === true) {
+            return true;
+        }
+    }
+    return false;
+}
